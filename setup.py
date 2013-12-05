1b7df866d694394309ac2c241a0df6f6c552b692
Guillaume Ardaud <gardaud@acm.org>
2013-12-04 20:50:03 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='hack-exe',
-    version='0.1',
+    version='0.2',
     url='https://github.com/gardaud/hack-exe',
     author='Guillaume Ardaud',
     author_email='gardaud@acm.org',
